943d64a5b2308fa40837c83b88d8e4cbcc398150
Kcssnow <kcssnow0109@gmail.com>
2025-11-21 09:36:02 +0800

    new file:   .gitignore new file:   index.html new file:   package.json new file:   src/App.jsx new file:   src/components/.gitkeep new file:   src/index.css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>LegalLink</h1>
+      <p>Welcome to LegalLink</p>
+    </div>
+  )
+}
+
+export default App
